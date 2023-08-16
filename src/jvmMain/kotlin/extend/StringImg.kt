0a24cfd98f7c3d92d6d28ced8c8cafde0136e77d
rafaelxx1 <rafaelsantosfr@hotmail.com>
2023-08-16 13:49:15 -0300

    step 2 - kotlin challenge, doc

--- src/jvmMain/kotlin/extend/StringImg.kt
+++ src/jvmMain/kotlin/extend/StringImg.kt
@@ -8,14 +8,15 @@ import java.net.URL
 import javax.imageio.ImageIO
 
 fun String.getImgBitmap(): ImageBitmap {
-    val url = URL(this)
-    val connection = url.openConnection() as HttpURLConnection
-    connection.doInput = true
-    connection.connect()
+    val url = URL(this) // transform the url string in URL
+    val connection = url.openConnection() as HttpURLConnection // make a connection with the URL,
+                                                              // and specified as HttpUrlConnection
+    connection.doInput = true // make sure that will do the Input
+    connection.connect() // make the Connection
 
 
-    val inputStream = connection.inputStream
-    val bufferedImg: BufferedImage = ImageIO.read(inputStream)
+    val inputStream = connection.inputStream // it reads the inputstream by the early connection
+    val bufferedImg: BufferedImage = ImageIO.read(inputStream) // read the image stream.
 
-    return bufferedImg.toComposeImageBitmap()
+    return bufferedImg.toComposeImageBitmap() // transform the bufferedimg to imageBitmap
 }
