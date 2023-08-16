3ef6d33246dcd5bf217cd7e4223af3e59c2efa8d
rafaelxx1 <rafaelsantosfr@hotmail.com>
2023-08-16 14:22:27 -0300

    step 2 - kotlin challenge, movielist

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -18,11 +18,13 @@ import javax.imageio.ImageIO
 @Composable
 @Preview
 fun App() {
-    val path ="https://sm.ign.com/ign_br/cover/h/harry-pott/harry-potter-the-series_s6p5.jpg"
+
 
     MaterialTheme {
-        Column {
-            myCard(file = path, title = "Harry Potter", score = 10, year = 2002) {}
+        Row {
+            movielist.forEach{movie ->
+                myCard(file = movie.url, title = movie.title, score = movie.score, year = movie.year) {}
+            }
         }
     }
 }
@@ -34,11 +36,20 @@ fun main() = application {
 }
 
 
+// just for test, isn't the final structure
+val url ="https://sm.ign.com/ign_br/cover/h/harry-pott/harry-potter-the-series_s6p5.jpg"
+val url2 = "https://s2.glbimg.com/Ts4sEmZsMfd-v5gDh4ev5IxXOZU=/e.glbimg.com/og/ed/f/original/2022/02/22/fmnz6aexiau47m8.jpg"
+val url3 = "https://m.media-amazon.com/images/M/MV5BMTU5MDg0NTQ1N15BMl5BanBnXkFtZTcwMjA4Mjg3Mg@@._V1_FMjpg_UX1000_.jpg"
+
+val movie1 = Movie("Harry Potter", url, 10, 2001 )
+val movie2 = Movie("Fantastic Beasts", url2, 10, 2017)
+val movie3 = Movie("Zombieland",url3, 10, 2010)
+val movielist: List<Movie> = listOf(movie1,movie2, movie3)
 
 
 data class Movie(
     val title: String,
-    val img: String,
+    val url: String,
     val score: Int,
     val year: Int
 ) {
